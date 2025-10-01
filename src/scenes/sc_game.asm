6be2c04c7518b728ae20cccfe05aa389ae5b1e5f
Marcos150 <marcosruizrubio@gmail.com>
2025-10-01 23:56:04 +0200

    ECS de clase. Falta corregir código

--- src/scenes/sc_game.asm
+++ src/scenes/sc_game.asm
@@ -1,15 +1,44 @@
+INCLUDE "constants.inc"
+INCLUDE "macros.inc"
+
+DEF VRAM_TILE_20 equ VRAM_TILE_START + ($20 * VRAM_TILE_SIZE)
+
 SECTION "Scene Game", ROM0
 
 sc_game_init::
-   call wait_vblank_start
-
-   ld a, DEFAULT_PAL
-   ld [rBGP], a
-   
-   ld hl, fence_tiles
-   ld de, VRAM_TILE_START + ($20 * VRAM_TILE_SIZE)
-   ld b, 2 * VRAM_TILE_SIZE
+   call man_entity_init
+   call man_entity_alloc
+   ;; HL: Component Address (write)
+   ld d, h
+   ld e, l
+   ld hl, sc_game_sprite_1
+   ld b, 4
    call memcpy_256
-	
+   call man_entity_alloc
+   ;; HL: Component Address (write)
+   ld d, h
+   ld e, l
+   ld hl, sc_game_sprite_2
+   ld b, 4
+   call memcpy_256
+
+
+   call lcd_off
+   SET_BGP DEFAULT_PAL
+   SET_OBP1 DEFAULT_PAL
+   MEMCPY_256 sc_game_fence_tiles, VRAM_TILE_20, 2*VRAM_TILE_SIZE
+
+   ld hl, rLCDC
+   set rLCDC_OBJ_ENABLE, [hl]
+   set rLCDC_OBJ_16x8, [hl]
+
+   call lcd_on
 
 	ret
+
+sc_game_run::
+   .loop:
+      call sys_render_update
+      ;; sys.....
+   jr .loop
+   ret
--- src/utils.asm
+++ src/utils.asm
@@ -2,17 +2,33 @@ INCLUDE "constants.inc"
 
 SECTION "Utils", ROM0
 
+;; LCD OFF
+;; DESTROYS: AF, HL
+lcd_off::
+   ;; BEWARE!!
+   call wait_vblank_start
+   ld hl, rLCDC
+   res rLCDC_LCD_ENABLE, [hl] ;; LCD OFF
+   ret
+
+;; LCD ON
+;; DESTROYS: AF, HL
+lcd_on::
+   ld hl, rLCDC
+   set rLCDC_LCD_ENABLE, [hl] ;; LCD ON
+   ret
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; VBLANK
 ;; DESTROYS AF, HL
 
 wait_vblank_start::
-      ld hl, rLY
-      ld a, VBLANK_START_LINE
-      .loop:
-            cp [hl]
-      jr nz, .loop
-      ret
+   ld hl, rLY
+   ld a, VBLANK_START_LINE
+   .loop:
+      cp [hl]
+   jr nz, .loop
+   ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; MEMCPY
@@ -30,4 +46,20 @@ memcpy_256::
       inc de
       dec b
    jr nz, memcpy_256
+   ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MEMSET 256
+;; INPUT:
+;; HL: Destination Address
+;; B: Bytes to set
+;; A: BValue to write
+;; 
+;; DESTROYS: AF, B, HL
+;;
+
+memset_256::
+      ld [hl+], a
+      dec b
+   jr nz, memset_256
    ret
