587acba14787a73d937ab3fac70f4d14908c4f4a
AlfonsoLafo <alfloplaf@gmail.com>
2025-10-10 20:07:13 +0200

    Movimiento terminado

--- src/systems/movement_system.asm
+++ src/systems/movement_system.asm
@@ -5,21 +5,6 @@ DEF SPEED equ %1
 SECTION "Movement System", ROM0
 
 movement_update::
-   ;; Left ladder X: 24
-   ;; Left ladder X: 44
-   ;; Spawn point X: 34, Y: 79
-   ;; Platforms  [Y: 79, Y: 61, Y: 49, Y: 31, Y: 19]
-   ;; Limits     {X: 10, X: 58}
-   ;; Falls      {X: 2B, X: 3C}
-
-   ;; TO-DO
-   ;; ----------------------------------------------------------
-   ;; IF Mauricio.x != 24 && Mauricio.x != 44: Disable UP_MOVE && DOWN_MOVE
-   ;; IF Mauricio.y => 19: Disable UP_MOVE
-   ;; IF Mauricio.y <= 79: Disable DOWN_MOVE
-   ;; IF Mauricio.x <= 10: Disable LEFT_MOVE
-   ;; IF Mauricio.x >= 58: Disable RIGHT_MOVE
-   ;; IF Mauricio.x >= 2B && Mauricio.x <= 3C: Disable ALL_MOVE && Mauricio.FALL
 
 
    check_prota_movement:
@@ -54,28 +39,90 @@ movement_update::
    .no_l:
       ret
 
-      move_r:
-         inc de
-         ld a, [de]
-         add a, SPEED
-         ld [de], a
-         ret
-
-      move_l:
-         inc de
-         ld a, [de]
-         sub a, SPEED
-         ld [de], a
-         ret
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ;; MOVIMIENTO DE MAURICIO EN LAS PLATAFORMAS
+   ;; ¡¡ Los números son hexadecimales !!
+   ;; Left ladder X: 24
+   ;; Left ladder X: 44
+   ;; Spawn point X: 34, Y: 79
+   ;; Platforms  [Y: 79, Y: 61, Y: 49, Y: 31, Y: 19]
+   ;; Limits     {X: 10, X: 58}
+   ;; Falls      {X: 2B, X: 3C}
 
       move_u:
-         ld a, [de]
-         sub a, SPEED
-         ld [de], a
+         inc de               ; Seleccionamos la x
+         ld a, [de]           ; Guardamos x en a para los cálculos
+         dec de               ; Volvemos a la y
+            cp $24            ; Comrobamos si su x está en la escalera izquierda
+            jr z, .move
+            cp $44            ; Comrobamos si su x está en la escalera derecha
+            jr nz, .no_ladder
+         .move:
+         ld a, [de]           ; Guardamos y en a 
+            cp $19            ; Si está en la plataforma superior no le dejamos moverse
+            jr z, .no_ladder
+         sub a, SPEED         ; Calculamos la nueva posición del sprite
+         ld [de], a           ; La guardamos
+         .no_ladder:
          ret
 
       move_d:
-         ld a, [de]
-         add a, SPEED
-         ld [de], a
+         inc de               ; Seleccionamos la x
+         ld a, [de]           ; Guardamos x en a para los cálculos
+         dec de               ; Volvemos a la y
+            cp $24            ; Comrobamos si su x está en la escalera izquierda
+            jr z, .move
+            cp $44            ; Comrobamos si su x está en la escalera derecha
+            jr nz, .no_ladder
+         .move:
+         ld a, [de]           ; Guardamos y en a 
+            cp $79            ; Si está en la plataforma inferior no le dejamos moverse
+            jr z, .no_ladder
+         add a, SPEED         ; Calculamos la nueva posición del sprite
+         ld [de], a           ; La guardamos
+         .no_ladder:
+         ret
+
+      move_r:
+         ld a, [de]           ; Guardamos y en a para los calculos
+         inc de               ; Seleccionamos la x
+            cp $79            ; Cada comprobación aquí es para la base de las plataformas
+            jr z, .move
+            cp $61            ; Estamos viendo si la y coincide con una plataforma
+            jr z, .move
+            cp $49
+            jr z, .move
+            cp $31
+            jr z, .move
+            cp $19
+            jr nz, .no_platform
+         .move:
+         ld a, [de]           ; Guardamos x en a
+         cp $58               ; Si está en el borde derecho no le dejamos moverse
+            jr z, .no_platform
+         add a, SPEED         ; Calculamos la nueva posición del sprite
+         ld [de], a           ; La guardamos
+         .no_platform:
+         ret
+
+      move_l:
+         ld a, [de]           ; Guardamos y en a para los calculos
+         inc de               ; Seleccionamos la x
+            cp $79            ; Cada comprobación aquí es para la base de las plataformas
+            jr z, .move
+            cp $61            ; Estamos viendo si la y coincide con una plataforma
+            jr z, .move
+            cp $49
+            jr z, .move
+            cp $31
+            jr z, .move
+            cp $19
+            jr nz, .no_platform
+         .move:
+         ld a, [de]           ; Guardamos x en a
+            cp $10            ; Si está en el borde izquierdo no le dejamos moverse
+            jr z, .no_platform
+         sub a, SPEED         ; Calculamos la nueva posición del sprite
+         ld [de], a           ; La guardamos
+         .no_platform:
          ret
